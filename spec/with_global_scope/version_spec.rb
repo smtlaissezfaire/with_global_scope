862f5835eaeeac1290f778c2927370dd976dde74
Scott Taylor <scott@railsnewbie.com>
2010-01-21 19:55:45 -0500

    Set version to 0.1.0

--- spec/with_global_scope/version_spec.rb
+++ spec/with_global_scope/version_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 describe WithGlobalScope do
   describe "VERSION" do
-    it "should be at 0.0.0" do
-      WithGlobalScope::Version::STRING.should == "0.0.0"
+    it "should be at 0.1.0" do
+      WithGlobalScope::Version::STRING.should == "0.1.0"
     end
 
     it "should have major as 0" do
       WithGlobalScope::Version::MAJOR.should == 0
     end
 
-    it "should have minor as 0" do
-      WithGlobalScope::Version::MINOR.should == 0
+    it "should have minor as 1" do
+      WithGlobalScope::Version::MINOR.should == 1
     end
 
     it "should have tiny as 0" do
